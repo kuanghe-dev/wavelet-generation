6beeed4fe9d985477d051b3bcf92cf6fee1bc16f
Kuang He <kuang.he@gmail.com>
2022-02-18 00:33:25 -0600

    Allow CORS

--- src/main/java/com/github/kuanghe_dev/waveletgen/WaveletGenerationApplication.java
+++ src/main/java/com/github/kuanghe_dev/waveletgen/WaveletGenerationApplication.java
@@ -2,9 +2,24 @@ package com.github.kuanghe_dev.waveletgen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class WaveletGenerationApplication {
+
+    @SuppressWarnings("deprecation")
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(WaveletGenerationApplication.class, args);
